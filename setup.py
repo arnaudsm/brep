5ed1198389dac4185a1cfd0b1c84b1c14391a078
arnaudsm <arnaudsm66@gmail.com>
2021-11-07 14:43:03 -0800

    1.0.1 : Added long-description from README

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+from pathlib import Path
 
 setup(
     name='brep',
-    version='1.0.0',
+    version='1.0.1',
     packages=['brep'],
     python_requires='>=3.5',
     scripts=['./cli/brep'],
@@ -11,6 +12,8 @@ setup(
     author='Arnaud de Saint Meloir (@arnaudsm)',
     author_email='arnaud.desaintmeloir@gmail.com',
     description='Binary Search in plaintext and Gzip files.',
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
     extras_require={
         'dev': ['pytest', 'flake8']
     }
